5d55a059f711e5368a0cde9c1777df2637ec7984
Wipfli Elias (U120010) <elias.wipfli@mobi.ch>
2020-11-02 14:51:16 +0100

    dist

---
+++ dist/precache-manifest.af10401d84ca1e3037dd877616c25155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d8ee2f8f662679845a",
+    "url": "css/app.e13e7bda.css"
+  },
+  {
+    "revision": "122e23845ca45bde460bcdfe06b12a99",
+    "url": "index.html"
+  },
+  {
+    "revision": "51d8ee2f8f662679845a",
+    "url": "js/app.f2b75001.js"
+  },
+  {
+    "revision": "0e095169c9f08821b2c3",
+    "url": "js/chunk-vendors.26f5c3c1.js"
+  },
+  {
+    "revision": "ac82b4d455f5b182b970dbc1f7e907db",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
